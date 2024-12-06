915dc02a22de411fa17f4e4025f9bbae6152a602
data4i0 <paulobiahu@gmail.com>
2024-12-06 13:26:06 +0100

    giving assessments

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     return render(request, 'blog/home_template.html')
+
+# create a view called home_view which points to a template called home_template.html 
+# welcome to Isreal Blog
+# create url for the blog
+# link the blog url to the main url
+
